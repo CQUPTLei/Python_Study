f66ffe7b932329bb16d1d620553131bcd1bc7634
CQUPTLei <1413454867@qq.com>
2022-03-31 22:05:17 +0800

    chap_2_1

--- basic_learn/py_2.py
+++ basic_learn/py_2.py
@@ -3,3 +3,28 @@
 # @Author : CQUPTLei
 # @File : py_2.py
 # @Softeare : PyCharm
+
+
+'''
+条件判断
+非 0 和 非空 为True
+0 和 None 为False
+'''
+
+if True:
+    print("True")
+else:
+    print("False")
+
+score = int(input("输入成绩:"))
+if score >= 90 and score <= 100:
+    print("等级为：A")
+elif score >= 80 and score < 90:
+    print("等级为：B")
+else:
+    print("等级为：C")
+
+
+import random
+x=random.randint(0,100)   #[0,100]之间的随机数
+print(x)
