9c71071f846b676b8686c030fee8ea91ac27cedd
dahu <1413454867@qq.com>
2024-07-02 17:07:18 +0800

    last change for yt-dlp

--- VideoDownload/DLP_GUI_Perfect.py
+++ VideoDownload/DLP_GUI_Perfect.py
@@ -4,11 +4,12 @@ from tkinter import filedialog
 import yt_dlp
 import threading
 
+# 打包命令示例，安装pyinstaller，路径自己更改
 # pyinstaller -F --paths=C:\Users\14134\.conda\envs\ytdlp\Lib\site-packages --python=C:\Users\14134\.conda\envs\ytdlp\pythonw.exe  --noconsole  --icon=1.ico --name=Downloader DLP_GUI_Perfect.py
 
 
 # 默认参数
-DEFAULT_URL = 'Please input a video url'
+DEFAULT_URL = ''
 DEFAULT_VIDEO_FORMAT = 'bestvideo'
 DEFAULT_AUDIO_FORMAT = 'bestaudio'
 DEFAULT_PATH = r'F:\download'
